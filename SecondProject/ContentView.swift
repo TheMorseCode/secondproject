cde82c6dd194c8dcb131fcc5ae184f4f1f3d01e6
Philinda Morse <philinda83@gmail.com>
2021-12-28 19:56:32 -0500

    added text

--- SecondProject/ContentView.swift
+++ SecondProject/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             if isTextShowing {
-                Text("Hello, world!")
+                Text("Hello, world! Xcode 12 Branch")
                     .padding()
             } else {
                 /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
